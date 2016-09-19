2eb8df3353d7b5fc4c12684ad6fa75e7712f3a54
daisuke shiraishi <daisuke_shiraishi@lightcafe.co.jp>
2016-09-19 17:44:15 +0900

    add base template

--- buono/views.py
+++ buono/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Generation
 
 # Create your views here.
 def index(request):
-    context = {}
+    generations = Generation.objects.order_by('-sortNo')
+    context = {'generations' : generations}
+    return render(request, 'buono/index.html', context)
+
+def listForGeneration(request, generation_id):
+    generations = Generation.objects.order_by('-sortNo')
+    context = {'generations' : generations}
     return render(request, 'buono/index.html', context)
