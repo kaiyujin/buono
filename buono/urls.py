fe70952c96842882cefe94cfa360c8760801ac9e
daisuke shiraishi <daisuke_shiraishi@lightcafe.co.jp>
2016-09-19 20:15:03 +0900

    add link to detail

--- buono/urls.py
+++ buono/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<generation_id>[0-9]+)/$', views.listForGeneration, name='listForGeneration'),
+    url(r'^appealPoint/(?P<appealPointId>[0-9]+)/$', views.detail, name='appealPoint'),
 ]
--- buono/views.py
+++ buono/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Generation
+from .models import AppealPoint
 
-# Create your views here.
 def index(request):
     generations = Generation.objects.order_by('-sortNo')
-    context = {'generations' : generations}
+    generation = generations[0]
+    appealPoints = AppealPoint.objects.filter(generation_id=generation.id).order_by('-updTm').select_related()
+    context = {
+        'generations'  : generations,
+        'generation'   : generation,
+        'appealPoints' : appealPoints,
+    }
     return render(request, 'buono/index.html', context)
 
 def listForGeneration(request, generation_id):
     generations = Generation.objects.order_by('-sortNo')
-    context = {'generations' : generations}
+    generation = get_object_or_404(Generation, pk=generation_id)
+    appealPoints = AppealPoint.objects.filter(generation_id=generation_id).order_by('-updTm').select_related()
+    context = {
+        'generations'  : generations,
+        'generation'   : generation,
+        'appealPoints' : appealPoints,
+    }
     return render(request, 'buono/index.html', context)
+
+def detail(request, appealPointId):
+    appealPoint = get_object_or_404(AppealPoint, pk=appealPointId)
+    context = {
+        'appealPoint'  : appealPoint,
+    }
+    return render(request, 'buono/detail.html', context)
