879f7fc5739809c37d725ef93c7f4079877ae77f
daisuke shiraishi <daisuke_shiraishi@lightcafe.co.jp>
2016-09-25 13:25:52 +0900

    change settings

--- common/settings.py
+++ common/settings.py
@@ -165,7 +165,6 @@ if os.environ.get('PRODUCTION') == 'True':
     DATABASES = {
         'default': dj_database_url.config(),
     }
-    DEBUG = False
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # Allow all host headers
