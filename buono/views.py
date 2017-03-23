6fd81b8036747dca37d669a4adbece56f530cb9e
Daisuke Shiraishi <kaiyujin@gmail.com>
2017-03-23 09:55:16 +0900

    投票期間変更

--- buono/views.py
+++ buono/views.py
@@ -7,7 +7,7 @@ import logging
 from datetime import date
 
 logger = logging.getLogger('model')
-isVoteTerm = date(2016, 10, 4) <= date.today()
+isVoteTerm = date(2017, 4, 3) <= date.today()
 #isVoteTerm = True #test
 #isVoteTerm = False #test
 
