64d40037b2e5dcf7e2f67326bb19a626735d0e1c
Daisuke Shiraishi <kaiyujin@gmail.com>
2017-09-14 14:32:13 +0900

    Update views.py

--- buono/views.py
+++ buono/views.py
@@ -7,8 +7,8 @@ import logging
 from datetime import date
 
 logger = logging.getLogger('model')
-#isVoteTerm = date(2017, 3, 29) <= date.today()
-isVoteTerm = True #test
+isVoteTerm = date(2017, 10, 2) >= date.today()
+#isVoteTerm = True #test
 #isVoteTerm = False #test
 
 @login_required
