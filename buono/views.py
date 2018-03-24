0597664d897317d1bcc1105e27faa03876cbee0b
daisuke shiraishi <kaiyujin@gmail.com>
2018-03-25 00:58:33 +0900

    文字数制限追加

--- buono/views.py
+++ buono/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
-from .models import AppealPoint,Comment,Vote
+from .models import AppealPoint, Comment, Vote
 import logging
 from datetime import date
 
 logger = logging.getLogger('model')
 isVoteTerm = date(2018, 4, 3) <= date.today()
-#isVoteTerm = True #test
-#isVoteTerm = False #test
+# isVoteTerm = True #test
+# isVoteTerm = False #test
+
 
 @login_required
 def index(request):
@@ -22,27 +23,29 @@ def index(request):
     except ObjectDoesNotExist:
         pass
     try:
-        Vote.objects.get(typeCd='1' ,user_id=request.user.id)
+        Vote.objects.get(typeCd='1', user_id=request.user.id)
     except ObjectDoesNotExist:
         commitedBuono = False
     try:
-        Vote.objects.get(typeCd='2' ,user_id=request.user.id)
+        Vote.objects.get(typeCd='2', user_id=request.user.id)
     except ObjectDoesNotExist:
         commitedSemiBuono = False
     commnetCount = Comment.objects.filter(user_id=request.user.id).count()
-    part_timer = True if request.user.groups.filter(name='part_timer').exists() else False
+    part_timer = True if request.user.groups.filter(
+        name='part_timer').exists() else False
     isDmm = True if request.user.groups.filter(name='dmm').exists() else False
     context = {
-        'appealPoints' : appealPoints,
-        'isVoteTerm' : isVoteTerm,
-        'commitedBuono' : commitedBuono,
-        'commitedSemiBuono' : commitedSemiBuono,
-        'commnetCount' : commnetCount,
-        'mineId' : mineId,
-        'part_timer' : part_timer,
-        'dmm' : isDmm,
+        'appealPoints': appealPoints,
+        'isVoteTerm': isVoteTerm,
+        'commitedBuono': commitedBuono,
+        'commitedSemiBuono': commitedSemiBuono,
+        'commnetCount': commnetCount,
+        'mineId': mineId,
+        'part_timer': part_timer,
+        'dmm': isDmm,
     }
     return render(request, 'buono/index.html', context)
+
 
 @login_required
 def detail(request, appealPointId):
@@ -50,123 +53,148 @@ def detail(request, appealPointId):
     if not isVoteTerm and not isDmm:
         return HttpResponseRedirect("/buono/")
     appealPoint = get_object_or_404(AppealPoint, pk=appealPointId)
-    comment, buono, semiBuono, nextAp, prevAp = ('',None,None,None,None)
-    comments, buonoList, semiBuonoList = (None,None,None)
+    comment, buono, semiBuono, nextAp, prevAp = ('', None, None, None, None)
+    comments, buonoList, semiBuonoList = (None, None, None)
     alreadyBuono, alreadySemiBuono = (None, None)
-    part_timer = True if request.user.groups.filter(name='part_timer').exists() else False
+    part_timer = True if request.user.groups.filter(
+        name='part_timer').exists() else False
     try:
-        prevAp = AppealPoint.objects.get(pk=appealPoint.id+1)
+        prevAp = AppealPoint.objects.get(pk=appealPoint.id + 1)
     except ObjectDoesNotExist:
-        pass;
+        pass
     try:
-        nextAp = AppealPoint.objects.get(pk=appealPoint.id-1)
+        nextAp = AppealPoint.objects.get(pk=appealPoint.id - 1)
     except ObjectDoesNotExist:
         pass
     if request.user.id == appealPoint.id:
-        comments = Comment.objects.filter(appealPoint=appealPoint).order_by('-updTm')
-        buonoList = Vote.objects.filter(appealPoint_id=appealPoint.id,typeCd='1')
-        semiBuonoList = Vote.objects.filter(appealPoint_id=appealPoint.id,typeCd='2')
+        comments = Comment.objects.filter(
+            appealPoint=appealPoint).order_by('-updTm')
+        buonoList = Vote.objects.filter(
+            appealPoint_id=appealPoint.id, typeCd='1')
+        semiBuonoList = Vote.objects.filter(
+            appealPoint_id=appealPoint.id, typeCd='2')
     else:
         try:
-            comment = Comment.objects.get(appealPoint_id=appealPoint.id,user_id=request.user.id)
+            comment = Comment.objects.get(
+                appealPoint_id=appealPoint.id, user_id=request.user.id)
         except ObjectDoesNotExist:
-            pass;
+            pass
         try:
-            buono = Vote.objects.get(appealPoint_id=appealPoint.id,user_id=request.user.id,typeCd='1')
+            buono = Vote.objects.get(
+                appealPoint_id=appealPoint.id, user_id=request.user.id, typeCd='1')
         except ObjectDoesNotExist:
-            pass;
+            pass
         try:
-            alreadyBuono = Vote.objects.get(user_id=request.user.id,typeCd='1')
+            alreadyBuono = Vote.objects.get(user_id=request.user.id, typeCd='1')
         except ObjectDoesNotExist:
-            pass;
+            pass
         try:
-            semiBuono = Vote.objects.get(appealPoint_id=appealPoint.id,user_id=request.user.id,typeCd='2')
+            semiBuono = Vote.objects.get(
+                appealPoint_id=appealPoint.id, user_id=request.user.id, typeCd='2')
         except ObjectDoesNotExist:
-            pass;
+            pass
         try:
-            alreadySemiBuono = Vote.objects.get(user_id=request.user.id,typeCd='2')
+            alreadySemiBuono = Vote.objects.get(
+                user_id=request.user.id, typeCd='2')
         except ObjectDoesNotExist:
-            pass;
+            pass
     mineId = None
     try:
         mineId = AppealPoint.objects.get(user_id=request.user.id).id
     except ObjectDoesNotExist:
         pass
     context = {
-        'appealPoint'  : appealPoint,
-        'comments'     : comments,
-        'nextAp'     : nextAp,
-        'prevAp'     : prevAp,
-        'isVoteTerm' : isVoteTerm,
-        'isMine' : mineId == appealPoint.id,
-        'buonoList' : buonoList,
-        'semiBuonoList' : semiBuonoList,
-        'comment' : comment,
-        'buono' : buono,
-        'semiBuono' : semiBuono,
-        'alreadyBuono' : alreadyBuono,
-        'alreadySemiBuono' : alreadySemiBuono,
-        'part_timer' : part_timer,
+        'appealPoint': appealPoint,
+        'comments': comments,
+        'nextAp': nextAp,
+        'prevAp': prevAp,
+        'isVoteTerm': isVoteTerm,
+        'isMine': mineId == appealPoint.id,
+        'buonoList': buonoList,
+        'semiBuonoList': semiBuonoList,
+        'comment': comment,
+        'buono': buono,
+        'semiBuono': semiBuono,
+        'alreadyBuono': alreadyBuono,
+        'alreadySemiBuono': alreadySemiBuono,
+        'part_timer': part_timer,
     }
     return render(request, 'buono/detail.html', context)
 
+
 @login_required
 def update(request):
-    if isVoteTerm :
+    if isVoteTerm:
         return HttpResponseRedirect("/buono/")
     appealPoint = None
-    message = ''
+    message = None
     try:
         appealPoint = AppealPoint.objects.get(user_id=request.user.id)
     except ObjectDoesNotExist:
         appealPoint = AppealPoint()
     if request.method == 'POST':
-        appealPoint.task    = request.POST['task']
+        appealPoint.task = request.POST['task']
         appealPoint.process = request.POST['process']
-        appealPoint.result  = request.POST['result']
-        appealPoint.force   = request.POST['force']
-        appealPoint.user    = request.user
-        appealPoint.save()
-        message = '更新しました。'
+        appealPoint.result = request.POST['result']
+        appealPoint.force = request.POST['force']
+        appealPoint.user = request.user
+        length = 100
+        if len(appealPoint.task.strip().strip('　')) >= length:
+            message = '1.取り組みは' + length + '文字以上書いてください'
+        if len(appealPoint.process.strip().strip('　')) >= length:
+            message = '2.プロセスは' + length + '文字以上書いてください'
+        if len(appealPoint.result.strip().strip('　')) >= length:
+            message = '3.成果は' + length + '文字以上書いてください'
+        if len(appealPoint.force.strip().strip('　')) >= length:
+            message = '4.理由は' + length + '文字以上書いてください'
+        if message = None:
+            message = '更新しました。'
+            appealPoint.save()
     context = {
-        'message':message,
-        'appealPoint' : appealPoint,
-        'isVoteTerm' : isVoteTerm,
+        'message': message,
+        'appealPoint': appealPoint,
+        'isVoteTerm': isVoteTerm,
     }
     return render(request, 'buono/update.html', context)
 
+
 @login_required
 def vote(request):
-    if not isVoteTerm :
+    if not isVoteTerm:
         return HttpResponseRedirect("/buono/")
     if request.method == 'GET':
         return HttpResponseRedirect("/buono/")
-    appealPoint = get_object_or_404(AppealPoint, pk=request.POST['appealPointId'])
+    appealPoint = get_object_or_404(
+        AppealPoint, pk=request.POST['appealPointId'])
     vote = Vote()
     vote.appealPoint = appealPoint
     vote.user = request.user
     vote.typeCd = request.POST['typeCd']
     vote.detail = request.POST['comment']
     vote.save()
-    return HttpResponseRedirect("/buono/"+request.POST['appealPointId']+"/")
+    return HttpResponseRedirect("/buono/" + request.POST['appealPointId'] + "/")
+
 
 @login_required
 def addComment(request):
-    if not isVoteTerm :
+    if not isVoteTerm:
         return HttpResponseRedirect("/buono/")
     if request.method == 'GET':
         return HttpResponseRedirect("/buono/")
-    appealPoint = get_object_or_404(AppealPoint, pk=request.POST['appealPointId'])
+    appealPoint = get_object_or_404(
+        AppealPoint, pk=request.POST['appealPointId'])
     comment = None
     try:
-        comment = Comment.objects.get(appealPoint_id=appealPoint.id,user_id=request.user.id)
+        comment = Comment.objects.get(
+            appealPoint_id=appealPoint.id, user_id=request.user.id)
     except ObjectDoesNotExist:
         comment = Comment()
         comment.appealPoint = appealPoint
-        comment.user   = request.user
+        comment.user = request.user
     comment.detail = request.POST['comment']
     comment.save()
-    return HttpResponseRedirect("/buono/"+request.POST['appealPointId']+"/")
+    return HttpResponseRedirect("/buono/" + request.POST['appealPointId'] + "/")
+
 
 @login_required
 def showComment(request):
@@ -211,11 +239,13 @@ on ap.user_id = us.id
     resultList = []
     tpl = list(raw)
     for val in tpl:
-        resultList.append({'typecd':val[0],'to':val[1],'from':val[2],'detail':val[3],})
+        resultList.append(
+            {'typecd': val[0], 'to': val[1], 'from': val[2], 'detail': val[3], })
     context = {
-        'raw' : resultList,
+        'raw': resultList,
     }
     return render(request, 'buono/show_comment.html', context)
+
 
 @login_required
 def countBuono(request):
@@ -246,9 +276,9 @@ ORDER BY typecd,cnt desc
     resultList = []
     tpl = list(raw)
     for val in tpl:
-        resultList.append({'typecd':val[0],'name':val[1],'cnt':val[2],})
+        resultList.append({'typecd': val[0], 'name': val[1], 'cnt': val[2], })
     context = {
-        'raw' : resultList,
+        'raw': resultList,
     }
     return render(request, 'buono/count_buono.html', context)
 
@@ -285,9 +315,9 @@ on users.id = voted.id
     resultList = []
     tpl = list(raw)
     for val in tpl:
-        resultList.append({'name':val[0]})
+        resultList.append({'name': val[0]})
     context = {
-        'raw' : resultList,
+        'raw': resultList,
     }
     return render(request, 'buono/yet_vote_list.html', context)
 
